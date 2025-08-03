9644fbb6ca41fc248636b5a30d7c23149948f801
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-08-03 22:04:32 +0100

    feat(home): display multiple Card components with dynamic title and content

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "@/components/layout/Header";
+import Card from "@/components/common/Card";
 
 const Home: React.FC = () => {
   return (
@@ -7,6 +8,18 @@ const Home: React.FC = () => {
       <Header />
       <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
         <h1>This is the Home Page</h1>
+
+        <div>
+          <Card
+            title="Top Villa"
+            content="Explore luxurious villas with great views."
+          />
+          <Card
+            title="Self Check-in"
+            content="Properties with flexible check-in options."
+          />
+          <Card title="Beachfront" content="Stay close to the ocean." />
+        </div>
       </main>
     </div>
   );
