b8af56662dca86c9f71099e31565beb6136231b9
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-08-06 04:13:28 +0100

    feat: fetch and display list of posts using PostCard in /posts page

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,14 +1,51 @@
+import React, { useEffect, useState } from "react";
 import Header from "@/components/layout/Header";
+import PostCard from "@/components/common/PostCard";
+import { type PostProps } from "@/interfaces";
 
 const PostsPage = () => {
-  return (
-    <>
-      <Header />
-      <div className="p-6">
-        <h1 className="text-2xl font-bold">Posts Page</h1>
-      </div>
-    </>
-  );
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  
+     useEffect(() => {
+       const fetchPosts = async () => {
+         const res = await fetch(
+           "https://jsonplaceholder.typicode.com/posts?_limit=10"
+         );
+         const data = await res.json();
+
+         const mappedPosts: PostProps[] = data.map((post: any) => ({
+           title: post.title,
+           content: post.body,
+           userId: post.userId,
+         }));
+
+         setPosts(mappedPosts);
+       };
+
+       fetchPosts();
+     }, []);
+    
+    return (
+      <>
+        <Header />
+        <div className="p-6">
+          <h1 className="text-2xl font-bold">Latest Posts</h1>
+
+          {posts.length === 0 ? (
+            <p>Loading posts...</p>
+          ) : (
+            posts.map((post, index) => (
+              <PostCard
+                key={index}
+                title={post.title}
+                content={post.content}
+                userId={post.userId}
+              />
+            ))
+          )}
+        </div>
+      </>
+    );
 };
 
 export default PostsPage;
