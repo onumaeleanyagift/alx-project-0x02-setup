4850061c6fe13c87411728ec7af1ba886b6d83c8
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-08-06 03:44:02 +0100

    feat: add Button examples to /about page with different sizes and shapes

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
 
 const About: React.FC = () => {
   return (
@@ -7,6 +8,12 @@ const About: React.FC = () => {
       <Header />
       <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
         <h1>This is the About Page</h1>
+
+        <div className="space-x-4">
+          <Button label="Small Rounded" size="small" shape="rounded-sm" />
+          <Button label="Medium Rounded" size="medium" shape="rounded-md" />
+          <Button label="Large Circle" size="large" shape="rounded-full" />
+        </div>
       </main>
     </div>
   );
