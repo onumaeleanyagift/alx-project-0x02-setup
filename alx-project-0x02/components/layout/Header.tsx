c82128cce2f30d320c6e9a781199cd5944866404
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-08-06 03:49:21 +0100

    feat: integrate Header component in home, about, and posts pages

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -5,13 +5,16 @@ const Header: React.FC = () => {
   return (
     <header className="bg-blue-600 text-white shadow-md py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <nav>
+        <nav className="flex space-x-6">
           <ul>
             <li>
               <Link href="/home">Home</Link>
             </li>
             <li>
               <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/posts">Post</Link>
             </li>
           </ul>
         </nav>
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/layout/Header";
+
+const PostsPage = () => {
+  return (
+    <>
+      <Header />
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Posts Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default PostsPage;
