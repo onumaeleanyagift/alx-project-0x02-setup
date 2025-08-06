232ba83ec8d2cc1e604a8f1e135f034861ed9628
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-08-06 04:16:35 +0100

    feat: fetch and display list of posts using PostCard in /posts page

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -2,50 +2,51 @@ import React, { useEffect, useState } from "react";
 import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 import { type PostProps } from "@/interfaces";
+import { getStaticProps } from "next/dist/build/templates/pages";
 
 const PostsPage = () => {
   const [posts, setPosts] = useState<PostProps[]>([]);
-  
-     useEffect(() => {
-       const fetchPosts = async () => {
-         const res = await fetch(
-           "https://jsonplaceholder.typicode.com/posts?_limit=10"
-         );
-         const data = await res.json();
 
-         const mappedPosts: PostProps[] = data.map((post: any) => ({
-           title: post.title,
-           content: post.body,
-           userId: post.userId,
-         }));
+  getStaticProps(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(
+        "https://jsonplaceholder.typicode.com/posts?_limit=10"
+      );
+      const data = await res.json();
 
-         setPosts(mappedPosts);
-       };
+      const mappedPosts: PostProps[] = data.map((post: any) => ({
+        title: post.title,
+        content: post.body,
+        userId: post.userId,
+      }));
 
-       fetchPosts();
-     }, []);
-    
-    return (
-      <>
-        <Header />
-        <div className="p-6">
-          <h1 className="text-2xl font-bold">Latest Posts</h1>
+      setPosts(mappedPosts);
+    };
 
-          {posts.length === 0 ? (
-            <p>Loading posts...</p>
-          ) : (
-            posts.map((post, index) => (
-              <PostCard
-                key={index}
-                title={post.title}
-                content={post.content}
-                userId={post.userId}
-              />
-            ))
-          )}
-        </div>
-      </>
-    );
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Latest Posts</h1>
+
+        {posts.length === 0 ? (
+          <p>Loading posts...</p>
+        ) : (
+          posts.map((post, index) => (
+            <PostCard
+              key={index}
+              title={post.title}
+              content={post.content}
+              userId={post.userId}
+            />
+          ))
+        )}
+      </div>
+    </>
+  );
 };
 
 export default PostsPage;
