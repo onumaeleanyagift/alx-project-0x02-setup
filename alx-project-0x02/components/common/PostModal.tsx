f05a9469601aa9b29bbfa5a3980eec1c0d5e0d5f
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-08-06 03:35:46 +0100

    feat: create PostModal component with title and content form

---
+++ alx-project-0x02/components/common/PostModal.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { type PostModalProps } from "@/interfaces/index";
+
+const PostModal: React.FC<PostModalProps> = ({ isOpen, onClose, onSubmit }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit({ title, content });
+    setTitle("");
+    setContent("");
+    onClose();
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center z-50">
+      <div className="bg-white p-6 rounded-md w-96 shadow-lg">
+        <h2 className="text-xl font-semibold mb-4">Create a Post</h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="w-full p2 border rounded"
+            required
+          />
+          <textarea
+            name=""
+            id=""
+            placeholder="Content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="w-full p2 border rounder"
+          ></textarea>
+          <div>
+            <button
+              type="button"
+              onClick={onClose}
+              className="px-4 py-2 bg-gray-300 rounded"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="px-4 py-2 bg-blue-600 text-white rounded"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default PostModal;
