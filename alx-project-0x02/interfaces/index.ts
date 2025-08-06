38b48f129d40ae47106d801a32f69c13305531b5
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-08-06 03:42:37 +0100

    feat: define ButtonProps interface for reusable Button component

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -12,3 +12,10 @@ export interface Post {
     title: string;
     content: string;
 }
+
+export interface ButtonProps {
+  label: string;
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+  onClick?: () => void;
+}
