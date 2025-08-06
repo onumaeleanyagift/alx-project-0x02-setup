8c24e925b9da6ffd585d40dd06e616f6dfdab587
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-08-06 03:37:07 +0100

    feat: define Post interface in interfaces/index.ts for type safety

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -2,3 +2,13 @@ export interface CardProps {
     title: string;
     content: string;
 }
+
+export interface PostModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    onSubmit: (data: { title: string; content: string; }) => void;
+}
+export interface Post {
+    title: string;
+    content: string;
+}
